a1d7d6886c7d76088157297fc8927cc3bc31070f
Gundeep Singh <gundeepsingh@richmedia.com>
2022-07-12 15:20:49 -0400

    Added report generator

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,14 @@ module.exports = defineConfig({
     projectId: "uicbrn",
     viewportWidth: 1280,
     viewportHeight: 700,
-   
+    "reporter": "mochawesome",
+    "reporterOptions": {
+        "charts": true,
+        "overwrite": true,
+        "html": false,
+        "json": true,
+        "reportDir": "cypress/reports/"
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
